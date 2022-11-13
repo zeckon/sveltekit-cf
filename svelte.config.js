7348099e9e4e46363ca8aaf67cf6cbe3aa5f1a56
burak.dogruoz <burak.dogruoz@rakuten.com>
2022-11-13 15:55:42 +0900

    cf changes 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,10 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      precompress: false,
-    }),
-
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true,
-    },
-  },
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
